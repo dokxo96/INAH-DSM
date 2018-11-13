f9a59fa68545008789ae3ba114884d50b54b72a2
Diana Laura <38481528+dianalee96@users.noreply.github.com>
2018-11-13 10:34:10 -0700

    Ventanas para actores

--- INAH-DSM/src/inah/dsm/LoginView_Operador.java
+++ INAH-DSM/src/inah/dsm/LoginView_Operador.java
@@ -11,12 +11,12 @@ import static javax.swing.JOptionPane.showMessageDialog;
  *
  * @author dokxo
  */
-public class LoginView extends javax.swing.JFrame {
+public class LoginView_Operador extends javax.swing.JFrame {
 
     /**
      * Creates new form LoginView
      */
-    public LoginView () {
+    public LoginView_Operador () {
         initComponents();
         this.setLocationRelativeTo(null);
         UserTxt.requestFocus();
@@ -216,21 +216,23 @@ public class LoginView extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger( LoginView .class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(LoginView_Operador .class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger( LoginView .class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(LoginView_Operador .class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(LoginView .class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(LoginView_Operador .class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger( LoginView .class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(LoginView_Operador .class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new LoginView().setVisible(true);
+                new LoginView_Operador().setVisible(true);
             }
         });
     }
